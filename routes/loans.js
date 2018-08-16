129d3d0f4443e1fedbe87177ec969080f9a6f446
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-16 18:35:02 +0200

    Plug loan DB table to Loan view

--- routes/loans.js
+++ routes/loans.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Loans = require('../models').Loans;
 
 router.get('/', function(req, res, next) {
-  res.render('loans', { title: 'All loans', page: req.baseUrl });
+  Loans.findAll().then(function(loans) {
+    res.render('loans', {
+      title: 'All loans',
+      page: req.baseUrl,
+      loans: loans
+    });
+  });
 });
 
 router.get('/new', function(req, res, next) {
