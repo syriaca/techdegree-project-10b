25dff77701ced0c367ad5e51b46b2a4424191e73
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-08-19 15:09:21 +0200

    Refactor: ES6 arrow function patron routes

--- routes/patrons.js
+++ routes/patrons.js
@@ -3,21 +3,25 @@ const router = express.Router();
 const Patrons = require('../models').Patrons;
 
 /* GET: Show all patrons list */
-router.get('/', function(req, res, next) {
-  Patrons.findAll().then(function(patrons) {
-    res.render('patrons/index', {
-      title: 'All patrons',
-      patrons: patrons
-    });
+router.get('/', (req, res, next) => {
+  Patrons
+    .findAll()
+    .then(patrons => {
+      res.render('patrons/index', {
+        title: 'All patrons',
+        patrons: patrons
+      });
   });
 });
 
 /* GET: Show individual patron */
-router.get('/details/:id', function(req, res, next) {
-  Patrons.findById(req.params.id).then(function(patron) {
-    res.render('patrons/details', {
-      patron: patron
-    });
+router.get('/details/:id', (req, res, next) => {
+  Patrons
+    .findById(req.params.id)
+    .then(patron => {
+      res.render('patrons/details', {
+        patron: patron
+      });
   });
 });
 
@@ -30,13 +34,15 @@ router.post('/:id', (req, res, next) => {
 });
 
 /* GET: Show patron creation form page */
-router.get('/new', function(req, res, next) {
+router.get('/new', (req, res, next) => {
   res.render('patrons/new', { patron: Patrons.build(), title: 'New Patron' });
 });
 
 /* POST: Create a new patron */
-router.post('/', function(req, res, next){
-  Patrons.create(req.body).then(function(patron){
+router.post('/', (req, res, next) => {
+  Patrons
+    .create(req.body)
+    .then(patron => {
     res.redirect('/patrons/details/'+ patron.id);
   });
 });
